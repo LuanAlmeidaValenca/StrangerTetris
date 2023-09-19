8593ac6b8a5e48bea75f5c28e0746f95535d7b09
FilippiMnz <imperilkill12@gmail.com>
2023-09-19 10:58:50 -0300

    Version 0.0.1

---
+++ js/tetris.js
@@ -0,0 +1,44 @@
+class Tetris{
+    constructor(imaxeX, imageY, template){
+
+        this.imageY = imageY;
+        this.imaxeX = imageX;
+        this.template = template;
+    }
+
+    checarBaixo(){
+        
+    }
+
+    checarEsquerda(){
+        
+    }
+
+    checarDireita(){
+
+    }
+
+    MovimentarDireita(){
+
+    }
+
+    MovimentarEsquerda(){
+
+    }
+
+    MovimentarBaixo(){
+
+    }
+
+    MudarRotacao(){
+
+    }
+}
+
+
+const TamanhoImagemQuadrado = 24
+const tamanho = 40
+const framePorSegundo = 24
+const VelocidadeJogo = 5
+const canvas = document.getElementById("canvas")
+const image = document.getElementById("image")
